d21c737af758ef292f1af14560dc0048fdf50d29
razzuSharma <razzusharma008@gmail.com>
2024-09-18 22:07:22 +0545

    initializing project and creating landing page

---
+++ app/wardrobe/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Wardrobe = () => {
+  return (
+    <div>
+        <h1>Wardrobe</h1>
+        <p>This is the wardrobe page</p>
+    </div>
+  )
+}
+
+export default Wardrobe
